3ad2ce29f6e455ba561a81cfe1aa5cac73128143
Fairuz Sakhiy <sakhiyfairuz@gmail.com>
2024-03-09 18:38:58 +0700

    removed unused code

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,6 @@
 import { UserButton, auth, currentUser } from "@clerk/nextjs";
 
 export default async function HomePage() {
-  const user = await currentUser  
-
-  const { userId } = auth()
-
-  console.log(userId)
-  
   return(
     <div className="'h-screen">
       <UserButton />
